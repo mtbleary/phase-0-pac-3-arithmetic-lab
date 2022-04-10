60975e26683a57990ee9259e249bdb2aea812a12
matthew leary <matt-leary@live.co.uk>
2022-04-10 14:51:05 +0100

    commit tree 9717388cfc41aed4c6d4f123986aafab7f614c15

---
+++ index.js
@@ -0,0 +1,25 @@
+function add(a, b) {
+return a + b;
+}
+function subtract(a, b) {
+return a - b;
+}
+function multiply(a, b) {
+return a * b;
+}
+function divide(a, b) {
+return a / b;
+}
+let n = 2.22 ;
+function increment(n) {
+    return (++n);
+}
+function decrement(n) {
+    return (--n);
+}
+function makeInt(string) {
+   return parseInt(string, 10 );
+}
+function preserveDecimal(n) {
+    return parseFloat(n, 10 );
+ }
